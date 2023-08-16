d5096a79c7f840814dbeed4a5ca2364fc97dc55e
Olivier Hoareau <oha@greenberets.io>
2023-08-16 12:29:30 +0200

    feat: check if at least one recipient

--- email.go
+++ email.go
@@ -1,12 +1,16 @@
 package goemail
 
 import (
+	"errors"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ses"
 )
 
 //goland:noinspection GoUnusedExportedFunction
 func SendEmail(email *Email) (string, error) {
+	if len(email.Bcc) <= 0 && len(email.Cc) <= 0 && len(email.To) <= 0 {
+		return "", errors.New("no recipients specified")
+	}
 	sess, _ := session.NewSession()
 	svc := ses.New(sess)
 
